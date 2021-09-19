26d0b14c93c388676c2bdc2959099fe69062b2ff
Prem CHOTEPANIT <prem.ch@ku.th>
2021-09-19 15:17:20 +0000

    Preprocessing done

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,7 @@
 DATASET_PATH = "tmlcc-2021"
-TRAIN_PATH = f"{DATASET_PATH}/mof_cif_train"
-TEST_PATH = f"{DATASET_PATH}/mof_cif_pretest"
+CIF_TRAIN_PATH = f"{DATASET_PATH}/mof_cif_train"
+CIF_PRETEST_PATH = f"{DATASET_PATH}/mof_cif_pretest"
+
+PREPROCESSING_TRAIN_PATH = 'preprocessing_results/train'
+PREPROCESSING_PRETEST_PATH = 'preprocessing_results/pretest'
 
--- utils/functions.py
+++ utils/functions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 
 def get_cif_filepath(
@@ -10,3 +11,26 @@ def get_cif_filepath(
     """
     mof_df["cif_filepath"] = cif_path + "/" + mof_df["MOFname"] + '.cif'
     return mof_df
+
+
+def merge_metadata(df: pd.DataFrame):
+    """
+    Merge MOF with CIF metadata
+    """
+    metadata_list = []
+    for cif in df.cif:
+        metadata_list.append(cif['metadata'][1].values())
+    metadata_df = pd.DataFrame(metadata_list)
+    metadata_df.index = df.index
+    metadata_df.columns = cif['metadata'][0].values()
+    return pd.concat([df, metadata_df], axis=1)
+
+
+def read_pandas_pickle(path: str):
+    dataframe_list = []
+    for pickle_file in os.listdir(path):
+        print(pickle_file)
+        filepath = f"{path}/{pickle_file}".replace("//", "/")
+        dataframe_list.append(pd.read_pickle(filepath))
+
+    return pd.concat(dataframe_list).sort_index()
