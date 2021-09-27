9354e3954391aa63dfb1d5fd305f8857f1b76e68
Prem CHOTEPANIT <prem.ch@ku.th>
2021-09-27 04:48:33 +0000

    restrucutre

--- utils/classes/adapters.py
+++ utils/classes/adapters.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 from typing import List
+from scipy.spatial import ConvexHull
 
 
 class CIF2PandasAdapter:
@@ -53,18 +54,32 @@ class CIF2PandasAdapter:
         return loops
     
     def apply(self, cif_filepath: str) -> List[pd.DataFrame]:
-        """
-        Apply ETL pipeline
-        """
-        # Extract
-        cif_list = self.load_cif(cif_filepath)
+            """
+            Apply ETL pipeline
+            """
+            # Extract
+            cif_list = self.load_cif(cif_filepath)
 
-        # Transform
-        metadata = self.get_metadata(cif_list)
-        extract_loops = self.get_loops(cif_list)
+            # Transform
+            metadata = self.get_metadata(cif_list)
+            extract_loops = self.get_loops(cif_list)
+            xyz = pd.DataFrame(extract_loops[0])[['_atom_site_fract_x', '_atom_site_fract_y', '_atom_site_fract_z']]
+            convex_hull = ConvexHull(xyz)
 
-        # Load
-        return {
-            "metadata": metadata.to_dict(),
-            "loops": extract_loops
-        }
+            metadata.at[11, 0] = 'mof_convex_hull_area'
+            metadata.at[11, 1] = convex_hull.area
+
+            metadata.at[12, 0] = 'mof_convex_hull_volume'
+            metadata.at[12, 1] = convex_hull.volume
+
+            metadata.at[13, 0] = 'mof_convex_hull_npoints'
+            metadata.at[13, 1] = convex_hull.npoints
+  
+            metadata.at[14, 0] = 'mof_convex_hull_nsimplex'
+            metadata.at[14, 1] = convex_hull.nsimplex
+            
+            # Load
+            return {
+                "metadata": metadata.to_dict(),
+                "loops": extract_loops
+            }
